baf5276e71cbc056a6a6a8a60c15b2ed993b7b8e
Alexander Ermakov <ermakov.a.a@mail.ru>
2020-02-18 05:30:10 +0300

    lesson6_неЗакончено

--- Lesson_6/src/Client/Client.java
+++ Lesson_6/src/Client/Client.java
@@ -1,7 +1,43 @@
 package Client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
+        Socket socket = null;
 
+        final String IP_ADDRESS = "localhost";
+        final int PORT = 8189;
+
+        try {
+            socket = new Socket(IP_ADDRESS, PORT);
+            Scanner in = new Scanner(socket.getInputStream());
+
+                while (true) {
+                        String str = in.nextLine();
+
+                        if(str.equals("/end")) {
+                            System.out.println("Сервер отключен");
+                            break;
+                        }
+
+                        System.out.println(str);
+                }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
--- Lesson_6/src/Server/Server.java
+++ Lesson_6/src/Server/Server.java
@@ -1,7 +1,52 @@
 package Server;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
     public static void main(String[] args) {
-        
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запустился");
+
+            socket = server.accept();
+            System.out.println("Клиент подключился");
+
+            Scanner output = new Scanner(System.in);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            while (true) {
+                    String str = null;
+                    str = output.nextLine();
+
+                    out.println(str);
+
+                    if (str.equals("/end")) {
+                        System.out.println("Сервер отключен");
+                        break;
+                    }
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
