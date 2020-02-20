dcba405a3909a9e8cf27c84fc383684028af09da
Alexander Ermakov <ermakov.a.a@mail.ru>
2020-02-20 15:50:21 +0300

    lesson6_OverTask

--- Lesson_6/src/Client/Client.java
+++ Lesson_6/src/Client/Client.java
@@ -1,7 +1,5 @@
 package Client;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -17,23 +15,47 @@ public class Client {
         try {
             socket = new Socket(IP_ADDRESS, PORT);
             Scanner in = new Scanner(socket.getInputStream());
+            Scanner output = new Scanner(System.in);
+            PrintWriter msg = new PrintWriter(socket.getOutputStream(), true);
 
+            Thread inputStream = new Thread(() -> {
                 while (true) {
-                        String str = in.nextLine();
+                    String str = in.nextLine();
 
-                        if(str.equals("/end")) {
-                            System.out.println("Сервер отключен");
-                            break;
-                        }
-
-                        System.out.println(str);
+                    if(str.equals("/end")) {
+                        System.out.println("Работа завершена");
+                        msg.println("Клиент отключен");
+                        break;
+                    }
+                    System.out.println(str);
                 }
+            });
+
+            Thread outputStream = new Thread(() -> {
+                while (true) {
+                    String str = output.nextLine();
+
+                    msg.println(str);
+                }
+            });
+
+            inputStream.start();
+            outputStream.setDaemon(true);
+            outputStream.start();
+
+            try {
+                inputStream.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
 
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
                 socket.close();
+                System.out.println("Сокет закрылся");
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- Lesson_6/src/Server/Server.java
+++ Lesson_6/src/Server/Server.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Server {
@@ -18,32 +19,69 @@ public class Server {
             socket = server.accept();
             System.out.println("Клиент подключился");
 
+            Scanner in = new Scanner(socket.getInputStream());
             Scanner output = new Scanner(System.in);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            PrintWriter msg = new PrintWriter(socket.getOutputStream(), true);
 
-            while (true) {
-                    String str = null;
-                    str = output.nextLine();
+            Thread inputStream = new Thread(() -> {
+                while (true) {
+                    try {
+                        String str = in.nextLine();
 
-                    out.println(str);
-
-                    if (str.equals("/end")) {
-                        System.out.println("Сервер отключен");
+                        /**
+                         * если от клиента пришел /end - он отправляется обратно и обрабатывается на стороне клиента,
+                         * завершая только работу клиента
+                         */
+                        if(str.equals("/end")) {
+                            msg.println(str);
+                        } else {
+                            System.out.println(str); }
+                    } catch (NoSuchElementException e) {
                         break;
                     }
-            }
+                }
 
+            });
+
+            Thread outputStream = new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = output.nextLine();
+
+                        msg.println(str);
+                        /**
+                         * /end на стороне сервера завершает работу и клиента и сервера
+                         */
+                    if (str.equals("/end")) {
+                        break;
+                    }}
+                } catch (NoSuchElementException e){
+                    return;
+                }
+            });
+
+            inputStream.setDaemon(true);
+            inputStream.start();
+            outputStream.start();
+
+            try {
+                outputStream.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
                 socket.close();
+                System.out.println("Сокет закрылся");
             } catch (IOException e) {
                 e.printStackTrace();
             }
             try {
                 server.close();
+                System.out.println("Сервер отключен");
             } catch (IOException e) {
                 e.printStackTrace();
             }
